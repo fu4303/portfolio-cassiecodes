10c1eb136c2805b52220e4f084ff3ebfcf7ab0d7
Cassie Evans <cassie.evans@clearleft.com>
2020-06-12 14:54:04 +0100

    writing set up

--- src/js/night-mode.js
+++ src/js/night-mode.js
@@ -1,6 +1,5 @@
 // setting dark vars before DOM content is loading to avoid flicker
 (function() {
-  
   if (
     window.localStorage.darkMode === 'true' ||
     (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)
@@ -10,9 +9,9 @@
     document.documentElement.style.setProperty('--color-mint', 'hsl(316, 30%, 36%)');
     document.documentElement.style.setProperty('--color-highlight', 'hsl(240, 33%, 77%)');
     document.documentElement.style.setProperty('--color-lilac', 'hsl(263, 29%, 32%)');
-    document.documentElement.style.setProperty('--color-lilac-up', 'hsl(257, 17%, 41%)');
     document.documentElement.style.setProperty('--color-sherbet', 'hsl(260, 24%, 18%)');
     document.documentElement.style.setProperty('--color-light', 'hsl(274, 4%, 15%)');
+    document.documentElement.style.setProperty('--color-light-fade', 'hsl(274, 4%, 16%)');
     document.documentElement.style.setProperty('--color-light-down', 'hsl(210, 4%, 98%)');
     document.documentElement.style.setProperty(
       '--color-light-down-2',
